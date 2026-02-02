c74d4916654a9588c229d5565ddeb6b8e87d370f
Asanda <noelmajola@gmail.com>
2026-02-02 18:26:55 +0200

    feat: add favourites count

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom";
 import "../css/NavBar.css";
+import { useMovieContext } from "../contexts/MovieContext";
 
 function NavBar() {
+    const { favorites } = useMovieContext();
     return (
         <nav className="navbar">
             <div className="navbar-brand">Crunch</div>
             <div className="navbar-links">
                 <Link to="/" className="nav-link">Home</Link>
-                <Link to="/favorites" className="nav-link">Favorites</Link>
+                <Link to="/favorites" className="nav-link">Favorites ({favorites.length})</Link>
             </div>
         </nav>
     );
